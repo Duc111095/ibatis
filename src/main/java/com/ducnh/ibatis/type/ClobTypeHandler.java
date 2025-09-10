a0727b2842a78cfc182402ced86e496fca5f9dc1
Duc111095 <huynhduc111095@gmail.com>
2025-09-11 00:11:40 +0700

    add type package

---
+++ src/main/java/com/ducnh/ibatis/type/ClobTypeHandler.java
@@ -0,0 +1,42 @@
+package com.ducnh.ibatis.type;
+
+import java.io.StringReader;
+import java.sql.CallableStatement;
+import java.sql.Clob;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ClobTypeHandler extends BaseTypeHandler<String>{
+	
+	public static final ClobTypeHandler INSTANCE = new ClobTypeHandler();
+	
+	@Override
+	public void setNonNullParameter(PreparedStatement ps, int i, String parameter, JdbcType jdbcType)
+		throws SQLException {
+		StringReader reader = new StringReader(parameter);
+		ps.setCharacterStream(i, reader, parameter.length());
+	}
+	
+	@Override
+	public String getNullableResult(ResultSet rs, String columnName) throws SQLException {
+		Clob clob = rs.getClob(columnName);
+		return toString(clob);
+	}
+	
+	@Override
+	public String getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+		Clob clob = rs.getClob(columnIndex);
+		return toString(clob);
+	}
+	
+	@Override
+	public String getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+		Clob clob = cs.getClob(columnIndex);
+		return toString(clob);
+	}
+	
+	private String toString(Clob clob) throws SQLException {
+		return clob == null ? null : clob.getSubString(1, (int) clob.length());
+	}
+}
