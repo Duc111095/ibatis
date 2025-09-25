8e4e7815b6e8e0f2bfb53aa13ac8a34884208015
Duc111095 <huynhduc111095@gmail.com>
2025-09-25 07:15:22 +0700

    add parameterHandler interface

---
+++ src/main/java/com/ducnh/ibatis/executor/parameter/ParameterHandler.java
@@ -0,0 +1,15 @@
+package com.ducnh.ibatis.executor.parameter;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ * A parameter handler sets the parameters of the {@code PreparedStatement}
+ * 
+ */
+public interface ParameterHandler {
+
+	Object getParameterObject();
+	
+	void setParameters(PreparedStatement ps) throws SQLException;
+}
